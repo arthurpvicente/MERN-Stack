84a9446b4f31bada4b53616075f4ff00f9ade96b
arthurpvicente <arthur.p.v@hotmail.com>
2025-07-01 18:16:46 -0300

    added frontend in production

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
+// Import database connection
 import { connectDB } from "./config/db.js";
 
+// Load environment variables
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
@@ -9,9 +13,21 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to accept JSON data from request body
 
 app.use("/api/products", productRoutes);
+
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    // Handle client-side routing
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 
 app.listen(PORT, () => {
